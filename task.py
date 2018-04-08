e5e6d05bb090f8a6ed3d43550cb99b17e675237c
mic19 <36995552+mic19@users.noreply.github.com>
2018-04-08 19:53:06 +0200

    Update task.py

--- task.py
+++ task.py
@@ -22,68 +22,90 @@
 #Your program must be runnable with command "python task.py".
 #Show some usecases of your library in the code (print some things)
 
+import statistics
+import json
+
 students = {}
 
 diary = {}
-grades = {"1":[], "2":[], "3":[]}
-diary["math"] = grades
-
-grades = {"1":[], "2":[], "3":[]}
-diary["english"] = grades
+diary["math"] = {}
+diary["english"] = {}
 
 def add_student(students, name, surname):
 	students[len(students) + 1] = (name, surname)
-	return str(len(students))
+	return len(students)
 	
-def assign_subjects(index, diary, *args):
+def assign_subjects(diary, index, *args):
 	for subject in args:
-		temp = diary[subject]
-		temp[index] = []
-	
-def get_student(students, index):
-	return students[index]
+		diary[subject][index] = {}
+		diary[subject][index]["grades"] = []
+		diary[subject][index]["attendance"] = []
 	
 def add_grade(diary, index, subject, *args):
 	for grade in args:
-		diary[subject][index].append(grade)
+		diary[subject][index]["grades"].append(grade)
 	
 def get_grades(diary, index, subject = None):
 	if subject == None:
 		grades_across = []
 		for subject in diary:
-			grades_across += diary[subject][index]
+			grades_across += diary[subject][index]["grades"]
 		return grades_across
-	return diary[subject][index]
+	return diary[subject][index]["grades"]
 
-def add_attendace(diary, index, subject):
-	pass
+def add_attendace(diary, index, subject, *args):
+	for attend in args:
+		diary[subject][index]["attendance"].append(attend)
 		
 def get_attendance(diary, index, subject):
-	pass
-	
+	return diary[subject][index]["attendance"]
 
+def save_to_file(diary, students, path):	
+	dict_to_save = {}
+	dict_to_save["diary"] = diary
+	dict_to_save["students"] = students
+	
+	j = json.dumps(dict_to_save)
+	f = open(path, "w")
+	f.write(j)
+	f.close()
+	
+def load_from_file(path):
+	dict_from_file = {}
+	with open(path, "r") as f:
+		dict_from_file = json.loads(f.read())
+	diary = dict_from_file["diary"]
+	students = dict_from_file["students"]
 
 if __name__ == "__main__":
+	print("Example\nBefore assigning students:")
 	print(diary)
 	index = add_student(students, "Jeremy", "Waterfall")
-	#assign_subjects(diary, index, "math", "english")
+	assign_subjects(diary, index, "math", "english")
 	index = add_student(students, "Joseph", "Parker")
-	#assign_subjects(diary, index, "math", "english")
-	index = add_student(students, "Adam", "Savage")
-	#assign_subjects(diary, index, "math", "english")
+	assign_subjects(diary, index, "math", "english")
 	
-	add_grade(diary, "1", "math", 2, 3, 4)
-	add_grade(diary, "2", "math", 4, 4, 5)
-	add_grade(diary, "3", "math", 5, 4, 5)
-	
-	add_grade(diary, "1", "english", 2, 4, 5)
-	add_grade(diary, "2", "english", 4, 3, 5)
-	add_grade(diary, "3", "english", 5, 4, 5)
-	
-	print(students)
+	print("\nAfter assigning students:")
 	print(diary)
-	print()
-	
-	print(get_grades(diary, "1"))
+	add_grade(diary, 1, "math", 3, 4)
+	add_attendace(diary, 1, "math", True, True)
+	add_grade(diary, 2, "math", 4, 5)
+	add_attendace(diary, 2, "math", True, True)
 
+	add_grade(diary, 1, "english", 4, 5)
+	add_attendace(diary, 1, "english", True, True)
+	add_grade(diary, 2, "english", 3, 5)
+	add_attendace(diary, 2, "english", True, True)
+	
+	print("\nAfter giving grades:")
+	print(diary)
+	
+	temp = get_grades(diary, 1)
+	print("\nGrades of studnet with index 1 across all subjects:")
+	print(temp)
+	print("Average:")
+	print(statistics.mean(temp))
+	
+	save_to_file(diary, students, "task.json")
+	
 
